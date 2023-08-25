e5f4b7f6fcdd2a81f1e9a3deb5df86fc959f7a11
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-25 00:04:51 -0500

    agrego-parte-cartsen-main.js

--- bootstrap-5.3.1-examples/pricing/main.js
+++ bootstrap-5.3.1-examples/pricing/main.js
@@ -13,5 +13,24 @@ let construirEncabezado = async()=>{
 
     `);
 }
+/**Div contendores */ 
+let construirDivContenerdor  = async()=>{
+    let peticion = await fetch(`${parch}.json`);
+    let res = await peticion.json();
+    let myJsonCartas = document.querySelector("#myJsonCarta");
+    myJsonCartas.insertAdjacentHTML("beforeend",/*html*/`
+    <div class="card mb-4 rounded-3 shadow-sm">
+    <div class="card-header py-3">
+        <h4 class="my-0 fw-normal">${item.title}</h4>
+    </div>
+    <div class="card-body">
+        <h1 class="card-title pricing-card-title">${item.price}<small class="text-body-secondary fw-light"></small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+        ${item.features.map((feature) => `<li>${feature}</li>`).join("")}
+    </ul>
+    <button type="button" class="w-100 btn btn-lg btn-danger">Get started</button>
+    </div>
+    </div>`);
+}
 
 construirEncabezado();
