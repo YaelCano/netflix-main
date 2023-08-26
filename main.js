1a4ecd0537a96b9fd00bc941b7864b54a2d2af39
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-25 22:58:44 -0500

    Correcion de comillas

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let construirEncabezado =async()=>{
     let peticion = await fetch(`${path}.json `);
     let res = await peticion.json();
     let Selecion = document.querySelector("#myjsonSection");
-    Selecion.insertAdjacentHTML("beforeend",/*html*/ `
+    Selecion.insertAdjacentHTML("beforeend",/*html*/`
     <h1 class="display-6 fw-normal text-body-emphasis d-flex justify-content-start">${res.section.titulo}</h1>
         ${res.section.parrafo.map((value)=>/*html*/` 
         <p class="fs-6 text-body-secondary d-flex justify-content-start">${value.icon}${value.text}</p>`).join(" ")}
@@ -14,8 +14,8 @@ let construirCartas = async () => {
     let peticion = await fetch(`${path}.json`);
     let res = await peticion.json();
     let Selecion = document.querySelector("#cartas");
-    Selecion.insertAdjacentHTML("beforeend", /*html*/ `
-        ${res.cartas.cardcontent.map((value) => /*html*/ `
+    Selecion.insertAdjacentHTML("beforeend", /*html*/`
+        ${res.cartas.cardcontent.map((value) => /*html*/`
             <div class="col">
                 ${value.Body.Border.border_red}
                     ${value.Body.Border.background}
@@ -39,7 +39,7 @@ let construirTabla = async()=>{
     let peticion = await fetch(`${path}.json `);
     let res = await peticion.json();
     let Selecion = document.querySelector("#mytable");
-    Selecion.insertAdjacentHTML("beforeend",/*html*/ `
+    Selecion.insertAdjacentHTML("beforeend",/*html*/`
     <h2 class='display-6 text-center mb-4'>${res.Tabla.titulo}</h2>
     <div class="table-responsive">
         <table class="table text-center">
@@ -59,7 +59,7 @@ let construirTabla = async()=>{
                     <td scope="row" class="text-center  align-middle text-muted" >${value.columna3}</td>
                     <td scope="row" class="text-center align-middle  text-danger">${value.columna4}</td>
                 </tr>
-                ` ).join(" ")}
+                `).join(" ")}
             </tbody>
             </table>
         </div>
